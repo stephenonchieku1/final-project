55cbbc9f8d24dde18c194926aedd86d5795786c8
stephenonchieku1 <onchiekustephen901@gmail.com>
2024-05-31 11:34:19 +0300

    :art: create  fadeanimation widget

---
+++ lib/screens/fade_animation.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FadeAnimation extends StatefulWidget {
+  const FadeAnimation({super.key});
+
+  @override
+  State<FadeAnimation> createState() => _FadeAnimationState();
+}
+
+class _FadeAnimationState extends State<FadeAnimation> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
