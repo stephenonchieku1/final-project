6c322f4b5ef3e3f0d0dcaa647d8c5af84846e5d5
stephenonchieku1 <onchiekustephen901@gmail.com>
2024-05-31 12:06:03 +0300

    :art: :zap:Updated FadeAnimation widget to accept child and delay props, and implemented fade and translate animations. Refactored LoginScreen to use SafeArea, Padding, and Column widgets.

--- lib/screens/fade_animation.dart
+++ lib/screens/fade_animation.dart
@@ -1,15 +1,54 @@
 import 'package:flutter/material.dart';
 
 class FadeAnimation extends StatefulWidget {
-  const FadeAnimation({super.key});
+  const FadeAnimation({super.key, required this.child, required this.delay});
 
+  final Widget child;
+  final double delay;
   @override
   State<FadeAnimation> createState() => _FadeAnimationState();
 }
 
-class _FadeAnimationState extends State<FadeAnimation> {
+class _FadeAnimationState extends State<FadeAnimation>
+    with SingleTickerProviderStateMixin {
+  late AnimationController controller;
+  late Animation<double> animation;
+  late Animation<double> animation2;
+  @override
+  void initState() {
+    super.initState();
+
+    controller = AnimationController(
+        duration: Duration(milliseconds: (500 * widget.delay).round()),
+        vsync: this);
+
+    animation2 = Tween<double>(begin: -40, end: 0).animate(controller)
+      ..addListener(() {
+        setState(() {});
+      });
+
+    animation = Tween<double>(begin: 0, end: 1).animate(controller)
+      ..addListener(() {
+        setState(() {});
+      });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    controller.forward();
+
+    return Transform.translate(
+      offset: Offset(0, animation2.value),
+      child: Opacity(
+        opacity: animation.value,
+        child: widget.child,
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
   }
 }
