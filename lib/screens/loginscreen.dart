a466a1679b570bcdee637e93dd26af4a7135e1d0
stephenonchieku1 <onchiekustephen901@gmail.com>
2024-05-31 12:21:25 +0300

    :art: :sparkles:"Added import for FadeAnimation, added boolean flag to _LoginScreenState, and :fire: removed unnecessary whitespace in padding."

--- lib/screens/loginscreen.dart
+++ lib/screens/loginscreen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import  'fade_animation.dart';
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
 
@@ -8,13 +8,14 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  bool flag = true;
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
       backgroundColor: Colors.blue,
       body: SafeArea(
         child: Padding(
-          padding: EdgeInsets.all(10.0)),
+          padding:  EdgeInsets.all(10.0)),
           child: Column(
            mainAxisAlignment: MainAxisAlignment.spaceBetween,
            crossAxisAlignment: CrossAxisAlignment.start,
