9e9bf03672b62778671e5fab5bd749eb6968f38d
stephenonchieku1 <onchiekustephen901@gmail.com>
2024-05-31 14:48:07 +0300

    :sparkles:"Added CustomElevatedButton widget and updated LoginScreen with new FadeAnimation element"

--- lib/components/custom_widgets.dart
+++ lib/components/custom_widgets.dart
@@ -1,15 +1,17 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class CustomTextFormField extends StatefulWidget {
-   final String hinttext;
+  final String hinttext;
   final bool obsecuretext;
   final TextEditingController? controller;
-  const CustomTextFormField( {
-     super.key,
+  const CustomTextFormField({
+    super.key,
     required this.hinttext,
     required this.obsecuretext,
-    this.controller, 
-     });
+    this.controller,
+  });
 
   @override
   State<CustomTextFormField> createState() => _CustomTextFormFieldState();
@@ -18,17 +20,42 @@ class CustomTextFormField extends StatefulWidget {
 class _CustomTextFormFieldState extends State<CustomTextFormField> {
   @override
   Widget build(BuildContext context) {
-     return TextFormField(
-        controller: widget.controller,
-              obscureText: widget.obsecuretext,
-              decoration: InputDecoration(
-                  contentPadding: const EdgeInsets.all(18),
-                      border: OutlineInputBorder(
-                      borderSide: const BorderSide(color: Colors.black),
-                      borderRadius: BorderRadius.circular(12)),
-                  hintText: widget.hinttext,
-                  hintStyle: const TextStyle( fontSize: 15, fontFamily: 'Urbanist-Medium', color: Color(0xFF8391A1) )),  
-
+    return TextFormField(
+      controller: widget.controller,
+      obscureText: widget.obsecuretext,
+      decoration: InputDecoration(
+          contentPadding: const EdgeInsets.all(18),
+          border: OutlineInputBorder(
+              borderSide: const BorderSide(color: Colors.black),
+              borderRadius: BorderRadius.circular(12)),
+          hintText: widget.hinttext,
+          hintStyle: const TextStyle(
+              fontSize: 15,
+              fontFamily: 'Urbanist-Medium',
+              color: Color(0xFF8391A1))),
     );
+  }
+}
+
+class CustomElevatedButton extends StatefulWidget {
+  final String message;
+  final FutureOr<void> Function() function;
+  final Color? color;
+  const CustomElevatedButton({
+    super.key,
+    required this.message,
+    required this.function,
+    this.color = Colors.white,
+  });
+
+  @override
+  State<CustomElevatedButton> createState() => _CustomElevatedButtonState();
+}
+
+class _CustomElevatedButtonState extends State<CustomElevatedButton> {
+  bool loading = false;
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
   }
 }
