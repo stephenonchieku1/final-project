07086a3a679daa3fc80b655ca0982319d021bc6d
stephenonchieku1 <onchiekustephen901@gmail.com>
2024-05-31 12:54:07 +0300

    :art: "Added imports,  :lipstick: modified LoginScreen widget structure :art: added FadeAnimation widgets with text children."

--- lib/screens/loginscreen.dart
+++ lib/screens/loginscreen.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import  'fade_animation.dart';
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -15,37 +17,47 @@ class _LoginScreenState extends State<LoginScreen> {
       backgroundColor: Colors.blue,
       body: SafeArea(
         child: Padding(
-          padding:  EdgeInsets.all(10.0)),
+          padding:  EdgeInsets.all(10.0),
           child: Column(
            mainAxisAlignment: MainAxisAlignment.spaceBetween,
            crossAxisAlignment: CrossAxisAlignment.start,
            children: [
-            Padding(padding: EdgeInsets.all(12.0)),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-
-              ]
-            ),
-            Padding(padding: EdgeInsets.all(12.0)),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                
-              ]
-            ),
-            const SizedBox(
-              Heigt(15),
-            ),
-            Padding(padding: EdgeInsets.all(12.0)),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                
-              ]
-            )
+            FadeAnimation(
+               delay:1,
+               child: Text("clicked"),
+               ),
+             Padding(
+                padding: EdgeInsets.all(12.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    FadeAnimation(
+                      delay: 1.3,
+                      child: Text(
+                        "Welcome back! Glad ",
+                        style: TextStyle(
+                          fontSize: 30,
+                          fontFamily: "urbanist",
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    FadeAnimation(
+                      delay: 1.6,
+                      child: Text(
+                        "to see you, Again!",
+                        style: TextStyle(
+                          fontSize: 30,
+                          fontFamily: "urbanist",
+                          fontWeight: FontWeight.bold,
+                        )
+                      ),
+                    ),
+                  ],
+                ),
+              ),     
           ],
-        ),
+        )),
       ),
     );
   }
