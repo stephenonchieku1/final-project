f29e5d25f959533ba7b52c3f77eb8b12e29af7c0
stephenonchieku1 <onchiekustephen901@gmail.com>
2024-05-31 12:08:34 +0300

    "Refactored LoginScreen widget structure, replacing Stack with SafeArea, Padding, and Column widgets."

--- lib/screens/loginscreen.dart
+++ lib/screens/loginscreen.dart
@@ -12,11 +12,39 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     return const Scaffold(
       backgroundColor: Colors.blue,
-      body: Stack(
-        children: [
-         
-         
-        ],
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(10.0)),
+          child: Column(
+           mainAxisAlignment: MainAxisAlignment.spaceBetween,
+           crossAxisAlignment: CrossAxisAlignment.start,
+           children: [
+            Padding(padding: EdgeInsets.all(12.0)),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+
+              ]
+            ),
+            Padding(padding: EdgeInsets.all(12.0)),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                
+              ]
+            ),
+            const SizedBox(
+              Heigt(15),
+            ),
+            Padding(padding: EdgeInsets.all(12.0)),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                
+              ]
+            )
+          ],
+        ),
       ),
     );
   }
