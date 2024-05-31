2a17974b9e48814da62ec9bcfe3fc5f0c341c39f
stephenonchieku1 <onchiekustephen901@gmail.com>
2024-05-31 23:05:41 +0300

    "Added padding, column, and fade animations to the password change screen"

--- lib/screens/password_change.dart
+++ lib/screens/password_change.dart
@@ -26,7 +26,41 @@ class _SignupPageState extends State<SignupPage> {
             child: IconButton(onPressed: (){}, 
            icon: const Icon(
             CupertinoIcons.back,
-           )), ),
+           )
+           ),
+            ),
+            const Padding(
+                padding: EdgeInsets.all(12.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    FadeAnimation(
+                      delay: 0.9,
+                      child: Text(
+                        "Hello! Register to get  ",
+                        style:TextStyle(
+                              fontSize: 30,
+                              fontFamily: "Urbanist",
+                              fontWeight: FontWeight.bold,
+                             
+                            ),
+                      ),
+                    ),
+                    FadeAnimation(
+                      delay: 1.2,
+                      child: Text(
+                        "started",
+                        style: TextStyle(
+                            fontSize: 30,
+                            fontFamily: "Urbanist",
+                            fontWeight: FontWeight.bold,
+                           
+                          ),  
+                      ),
+                    ),
+                  ],
+                ),
+              ),
         ],
 
 
