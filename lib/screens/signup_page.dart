13d1f308132b3aaa872081cb7d43fef1ee71bc4a
stephenonchieku1 <onchiekustephen901@gmail.com>
2024-06-01 17:23:11 +0300

    :sparkles:"Added PasswordChange StatefulWidget with Placeholder widget"

---
+++ lib/screens/signup_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class PasswordChange extends StatefulWidget {
+  const PasswordChange({super.key});
+
+  @override
+  State<PasswordChange> createState() => _PasswordChangeState();
+}
+
+class _PasswordChangeState extends State<PasswordChange> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
