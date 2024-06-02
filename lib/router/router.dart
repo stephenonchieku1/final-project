e1ab9c63df185137c705a35fa4eff926fdd09935
stephenonchieku1 <onchiekustephen901@gmail.com>
2024-06-02 19:34:46 +0300

    :zap: added approuter class and routers

---
+++ lib/router/router.dart
@@ -0,0 +1,17 @@
+class AppRouter {
+
+  String name;
+  String path;
+  AppRouter({
+    required this.name,
+    required this.path,
+  });
+}
+class Routers {
+    static AppRouter loginpage =AppRouter(name: "/login", path: "/login");
+     static AppRouter signuppage =AppRouter(name: "/signup", path: "/signup");
+
+
+
+
+}
