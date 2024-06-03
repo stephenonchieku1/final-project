f1bc422cbd5818623e6ccb64af851e0c3d12139d
stephenonchieku1 <onchiekustephen901@gmail.com>
2024-06-03 19:00:05 +0300

    :art:added the  newpasssword route

--- lib/router/router.dart
+++ lib/router/router.dart
@@ -10,7 +10,8 @@ class AppRouter {
 class Routers {
     static AppRouter loginpage =AppRouter(name: "/login", path: "/login");
      static AppRouter signuppage =AppRouter(name: "/signup", path: "/signup");
-
+static AppRouter newpassword =
+      AppRouter(name: "/newpassword", path: "/newpassword");
 
 
 
