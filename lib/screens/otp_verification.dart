a01bcd2204fa6010cafea0772e04db3d2407bb64
stephenonchieku1 <onchiekustephen901@gmail.com>
2024-06-03 18:39:13 +0300

    "Added OtpVerification widget with StatefulWidget and State implementation returning a Placeholder widget."

---
+++ lib/screens/otp_verification.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/widgets.dart';
+
+class OtpVerification extends StatefulWidget {
+  const OtpVerification({super.key});
+
+  @override
+  State<OtpVerification> createState() => _OtpVerificationState();
+}
+
+class _OtpVerificationState extends State<OtpVerification> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
