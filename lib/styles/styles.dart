64e0da88401487c0c994a7f4ae2dad7ed32217ef
stephenonchieku1 <onchiekustephen901@gmail.com>
2024-06-04 23:56:26 +0300

    :lipstick: "Added Styles class with mediumBlack TextStyle definition"

---
+++ lib/styles/styles.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Styles {
+  TextStyle mediumBlack = const TextStyle(
+      fontSize: 16,
+      fontFamily: "Urbanist-Bold",
+      fontWeight: FontWeight.w300,
+      color: Colors.grey);
+}
