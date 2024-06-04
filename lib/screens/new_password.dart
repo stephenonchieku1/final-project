0d081050f4d826580ceaac092df1396287d7e9dc
stephenonchieku1 <onchiekustephen901@gmail.com>
2024-06-05 01:06:33 +0300

    "Updated NewPassword and OtpVerification screens with new UI components and navigation"

--- lib/screens/new_password.dart
+++ lib/screens/new_password.dart
@@ -1,7 +1,11 @@
+import 'package:finalproject/router/router.dart';
 import 'package:finalproject/screens/fade_animation.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:finalproject/components/custom_widgets.dart';
+import 'package:finalproject/styles/styles.dart';
 import 'package:go_router/go_router.dart';
+
 
 class NewPassword extends StatefulWidget {
   const NewPassword({super.key});
@@ -30,10 +34,70 @@ class _NewPasswordState extends State<NewPassword> {
                       CupertinoIcons.back,
                       size:35,
                     ),
-                  )
-                  )
-
-
+                  ),
+                  ),
+              Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    FadeAnimation(
+                      delay: 1.3,
+                      child: Text(
+                        "Create new password",
+                        style: Styles().titelTheme,
+                      ),
+                    ),
+                    FadeAnimation(
+                      delay: 1.6,
+                      child: Text(
+                        "Your new password must be unique from those previously used.",
+                        style: Styles().mediumBlack,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+                Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Form(
+                  child: Column(
+                    children: [
+                      const FadeAnimation(
+                        delay: 1.9,
+                        child: CustomTextFormField(
+                          hinttext: 'New password',
+                          obsecuretext: false,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 15,
+                      ),
+                      const FadeAnimation(
+                        delay: 2.1,
+                        child: CustomTextFormField(
+                          hinttext: 'Confirm password',
+                          obsecuretext: false,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      FadeAnimation(
+                        delay: 2.4,
+                        child: CustomElevatedButton(
+                          message: "Reset Password ",
+                          function: () {
+                            GoRouter.of(context)
+                                .pushNamed(Routers.passwordchanges.name);
+                          },
+                          color: Colors.black,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),        
                   
             ],
           ),
