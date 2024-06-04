ababcf5bd0f0a88e7256594e5c488a525c3af9ed
stephenonchieku1 <onchiekustephen901@gmail.com>
2024-06-05 00:09:51 +0300

    :lipstick: "Added 'titelTheme' TextStyle to Styles class and updated 'mediumBlack' TextStyle."

--- lib/styles/styles.dart
+++ lib/styles/styles.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
 
 class Styles {
+  TextStyle titelTheme = const TextStyle(
+    fontSize: 30,
+    fontFamily: "Urbanist",
+    fontWeight: FontWeight.bold,
+  );
+
   TextStyle mediumBlack = const TextStyle(
       fontSize: 16,
       fontFamily: "Urbanist-Bold",
       fontWeight: FontWeight.w300,
       color: Colors.grey);
+      
 }
