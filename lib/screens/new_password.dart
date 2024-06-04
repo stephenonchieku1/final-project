c02dce4660893f703351fcfcd2a9394800e27d82
stephenonchieku1 <onchiekustephen901@gmail.com>
2024-06-05 00:50:29 +0300

    "Added import statements and implemented basic UI structure for NewPassword screen"

--- lib/screens/new_password.dart
+++ lib/screens/new_password.dart
@@ -1,6 +1,7 @@
+import 'package:finalproject/screens/fade_animation.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
+import 'package:go_router/go_router.dart';
 
 class NewPassword extends StatefulWidget {
   const NewPassword({super.key});
@@ -12,6 +13,32 @@ class NewPassword extends StatefulWidget {
 class _NewPasswordState extends State<NewPassword> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              FadeAnimation(
+                  delay: 1,
+                  child: IconButton(
+                    onPressed: () {
+                      GoRouter.of(context).pop();
+                    },
+                    icon: const Icon(
+                      CupertinoIcons.back,
+                      size:35,
+                    ),
+                  )
+                  )
+
+
+                  
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
