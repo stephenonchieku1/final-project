13a1502a031d0328129909823fe424f115c035fc
stephenonchieku1 <onchiekustephen901@gmail.com>
2024-06-05 00:40:08 +0300

    :sparkles: "Added NewPassword StatefulWidget with Placeholder widget"

---
+++ lib/screens/new_password.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class NewPassword extends StatefulWidget {
+  const NewPassword({super.key});
+
+  @override
+  State<NewPassword> createState() => _NewPasswordState();
+}
+
+class _NewPasswordState extends State<NewPassword> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
