69f191ad20343baf6eb87b7c57d0a4f54e187e67
stephenonchieku1 <onchiekustephen901@gmail.com>
2024-06-05 00:33:03 +0300

    :lipstick: Added two new TextStyle properties to the Styles class: hintText and mediumTheme."

--- lib/styles/styles.dart
+++ lib/styles/styles.dart
@@ -12,5 +12,12 @@ class Styles {
       fontFamily: "Urbanist-Bold",
       fontWeight: FontWeight.w300,
       color: Colors.grey);
-      
+  TextStyle hintText = const TextStyle(
+      fontSize: 15, fontFamily: 'Urbanist-Medium', color: Color(0xFF8391A1));
+  TextStyle mediumTheme = const TextStyle(
+      fontSize: 15,
+      fontFamily: "Urbanist-Bold",
+      fontWeight: FontWeight.bold,
+      color: Color.fromARGB(255, 72, 151, 151)
+      );
 }
