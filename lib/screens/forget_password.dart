f28eef1a69d38d9e4d90b597b11d43a5c102b5ee
stephenonchieku1 <onchiekustephen901@gmail.com>
2024-06-04 23:43:30 +0300

    "Added imports and implemented UI for ForgetPassword screen"

--- lib/screens/forget_password.dart
+++ lib/screens/forget_password.dart
@@ -1,4 +1,7 @@
+import 'package:finalproject/screens/fade_animation.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class ForgetPassword extends StatefulWidget {
   const ForgetPassword({super.key});
@@ -10,6 +13,44 @@ class ForgetPassword extends StatefulWidget {
 class _ForgetPasswordState extends State<ForgetPassword> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+      body: SafeArea(
+          child: Padding(
+              padding: EdgeInsets.all(10.0),
+              child: Column(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.all(12.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        FadeAnimation(
+                          delay: 1,
+                          child: Text(
+                            "Forgot Password",
+                            style: TextStyle(
+                              fontSize: 30,
+                              fontFamily: "Urbanist",
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                        FadeAnimation(
+                          delay: 1,
+                          child: Text(
+                            "Please Enter the email address linked to your email",
+                            style: TextStyle(
+                              fontSize: 30,
+                              fontFamily: "Urbanist",
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ))),
+    );
   }
 }
