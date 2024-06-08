25945bff0476cb7cb77a1314f58e9e02e8ee263a
stephenonchieku1 <onchiekustephen901@gmail.com>
2024-06-08 23:58:23 +0300

    added new dependencies

---
+++ lib/models/trialpage.dart
@@ -0,0 +1,12 @@
+class Appointment {
+  String id;
+  String title;
+  
+  DateTime dateTime;
+
+  Appointment({required this.id, required this.title, required this.dateTime});
+
+  get date => null;
+
+  
+}
