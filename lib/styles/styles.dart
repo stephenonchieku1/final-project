dee99055d939e507b08c451a153f62067894405f
stephenonchieku1 <onchiekustephen901@gmail.com>
2024-06-10 17:09:02 +0300

    :lipstick:added coloe styles

--- lib/styles/styles.dart
+++ lib/styles/styles.dart
@@ -26,3 +26,30 @@ class Styles {
   Color black = const Color(0xFF1E232C);
 
 }
+
+
+Color primaryColor = Colors.blue.shade700;
+Color colorBlue = Colors.blue;
+Color colorDarkBlue = Colors.blue.shade700;
+Color colorRed = Colors.red;
+Color colorCyan = Colors.cyan.shade300;
+Color colorLightGrey =  Colors.black ;
+Color colorLightGrey100 = Colors.grey.shade100;
+Color colorGreyShade = const Color(0xfff5f5f5);
+
+const appColorPrimary = Color(0xFF1157FA);
+const iconColorPrimary = Color(0xFFFFFFFF);
+const iconColorSecondary = Color(0xFFA8ABAD);
+const appSecondaryBackgroundColor = Color(0xFF131d25);
+const appTextColorPrimary = Color(0xFF212121);
+const appTextColorSecondary = Color(0xFF5A5C5E);
+const appShadowColor = Color(0x95E9EBF0);
+const appColorPrimaryLight = Color(0xFFF9FAFF);
+
+// Dark Theme Colors
+const appBackgroundColorDark = Color(0xFF121212);
+const cardBackgroundBlackDark = Color(0xFF1F1F1F);
+const colorprimaryblack = Color(0xFF131d25);
+const iconColorPrimaryDark = Color(0xFF212121);
+const iconColorSecondaryDark = Color(0xFFA8ABAD);
+const appShadowColorDark = Color(0x1A3E3942);
