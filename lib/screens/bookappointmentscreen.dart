0d8641aac43626c6d8f5e8a290919d87d3df0ce6
stephenonchieku1 <onchiekustephen901@gmail.com>
2024-06-10 20:39:24 +0300

    create  bookappoinment screen

---
+++ lib/screens/bookappointmentscreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BookAppoinmentScreen extends StatefulWidget {
+  const BookAppoinmentScreen({super.key});
+
+  @override
+  State<BookAppoinmentScreen> createState() => _BookAppoinmentScreenState();
+}
+
+class _BookAppoinmentScreenState extends State<BookAppoinmentScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
