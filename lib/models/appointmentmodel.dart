a9201f09a2829bb8ee6c29f4df5dd9bc26751e39
stephenonchieku1 <onchiekustephen901@gmail.com>
2024-06-10 16:33:24 +0300

    "Added AppointmentModel class with 6 properties and a constructor"

---
+++ lib/models/appointmentmodel.dart
@@ -0,0 +1,16 @@
+class AppointmentModel{
+  String? service;
+  String? date;
+  String? month;
+  String? doctor;
+  String? patient;
+  String? department;
+
+AppointmentModel({this.service, 
+this.date,
+ this.month, 
+ this.doctor, 
+ this.patient,
+  this.department
+  });
+}
