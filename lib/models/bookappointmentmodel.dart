988d33d13752bdd8665fcec3df5144f8daad7e84
stephenonchieku1 <onchiekustephen901@gmail.com>
2024-06-10 20:44:12 +0300

    create bookappointment  model

---
+++ lib/models/bookappointmentmodel.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BookAppointmentModel {
+  String? id;
+  String? title;
+  Widget? widget;
+  double? progress;
+
+  BookAppointmentModel({this.id, this.title, this.widget, this.progress});
+}
