451ec2d87fc478b6107b77ccdae8a5e945da042f
stephenonchieku1 <onchiekustephen901@gmail.com>
2024-06-11 20:47:47 +0300

    added the sqlite flutter package

--- lib/utils/dataprovider.dart
+++ lib/utils/dataprovider.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:nb_utils/nb_utils.dart';
 import 'package:finalproject/models/bookappointmentmodel.dart';
