5cda423d9c7c196869472e09fc0e4db4e9816e89
stephenonchieku1 <onchiekustephen901@gmail.com>
2024-06-12 18:40:49 +0300

    "Added database functionality for dogs and users, including CRUD operations and UI components for user registration and listing."

---
+++ lib/screens/userscreen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:finalproject/models/user.dart';
+
+
+class UserDetailPage extends StatefulWidget {
+  const UserDetailPage({super.key, required this.user});
+  final User user;
+
+  @override
+  State<UserDetailPage> createState() => _UserDetailPageState();
+}
+
+class _UserDetailPageState extends State<UserDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('User Detail'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Name: ${widget.user.name}',
+              style: const TextStyle(fontSize: 20),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              'Email: ${widget.user.email}',
+              style: const TextStyle(fontSize: 18),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
